b5f79c37018a387eba593a5708738cb72dca9aff
Gabriel Dujardin <gabrielmdujardin@gmail.com>
2026-03-06 16:15:09 -0300

    Service layer, component registration

--- src/main/java/com/projectgabrielD/course/repositories/UserRepository.java
+++ src/main/java/com/projectgabrielD/course/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.projectgabrielD.course.repositories;
 
 import com.projectgabrielD.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
