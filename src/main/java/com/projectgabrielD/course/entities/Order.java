822705933d6230e6460b85f799eb25a1a3175025
Gabriel Dujardin <gabrielmdujardin@gmail.com>
2026-03-12 16:31:37 -0300

    Payment, one-to-one association

--- src/main/java/com/projectgabrielD/course/entities/Order.java
+++ src/main/java/com/projectgabrielD/course/entities/Order.java
@@ -31,7 +31,8 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
-
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -78,6 +79,13 @@ public class Order implements Serializable {
         }
     }
 
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 
     public Set<OrderItem> getItems(){
         return items;
