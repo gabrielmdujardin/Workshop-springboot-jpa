0d057aa63dc0e353f2fdb5add4200c15884922fe
Gabriel Dujardin <gabrielmdujardin@gmail.com>
2026-03-30 15:31:32 -0300

    Exeption handling update

--- src/main/java/com/projectgabrielD/course/services/UserService.java
+++ src/main/java/com/projectgabrielD/course/services/UserService.java
@@ -4,11 +4,13 @@ import com.projectgabrielD.course.entities.User;
 import com.projectgabrielD.course.repositories.UserRepository;
 import com.projectgabrielD.course.services.exceptions.DatabaseException;
 import com.projectgabrielD.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 import java.util.Optional;
 
@@ -46,9 +48,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw  new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
